eb219a532ea8c5fa2222a5b27a518a7561379bee
Jothi Priya Sundaravdivel <34646744+priya004@users.noreply.github.com>
2017-12-19 12:48:09 +0530

    Create Vowel_Consonent.c

---
+++ Codekata/Beginner/Set_1/Vowel_Consonent.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	char p;
+	printf("Enter a character:");
+	scanf("%c",&p);
+	if(p == 'a' || p == 'e' || p == 'i' || p == 'o' || p == 'u' || p == 'A' || p == 'E' || p == 'I' || p == 'O' || p == 'U')
+		printf("Vowel!");
+	else
+		printf("Constant!");
+	return 0;
+}
