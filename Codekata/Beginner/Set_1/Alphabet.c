51dc5df0c087b5038e2d5aa0510aa5789e6931f7
Jothi Priya Sundaravdivel <34646744+priya004@users.noreply.github.com>
2017-12-19 18:30:20 +0530

    Update Alphabet.c

--- Codekata/Beginner/Set_1/Alphabet.c
+++ Codekata/Beginner/Set_1/Alphabet.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    char c;
+    char p;
     printf("Enter a character: ");
-    scanf("%c",&c);
-
-    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
-        printf("%c is an alphabet.",c);
+    scanf("%c",&p);
+    if( (p >= 'a' && p <= 'z') || (p >= 'A' && p <= 'Z'))
+        printf("Alphabet");
     else
-        printf("%c is not an alphabet.",c);
-
+        printf("Not an alphabet");
     return 0;
 }
