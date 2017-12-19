4d8335292d9cf6aaf46caff261964b8ad66cf4c6
Jothi Priya Sundaravdivel <34646744+priya004@users.noreply.github.com>
2017-12-19 12:31:12 +0530

    Create Variables.c

---
+++ Guvi/Beginner/Variables.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	char name[25] = "Sachin Ramesh Tendulkar";
+    char nick[20] = "Little Master";
+    int runs = 18426 , matches = 463;
+    float avg = 44.83;
+    printf("%s\n%s\n%d\n%.2f\n%d",name,nick,runs,avg,matches);
+return 0;
+}
