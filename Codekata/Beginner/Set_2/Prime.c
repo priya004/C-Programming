fb7f226efb86c00a6bbce7148ab3605281d42f7f
Jothi Priya Sundaravdivel <34646744+priya004@users.noreply.github.com>
2018-02-23 11:49:36 +0530

    Update Prime.c

--- Codekata/Beginner/Set_2/Prime.c
+++ Codekata/Beginner/Set_2/Prime.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main(){
-	int n,a = 0,flag = 0,i;
+	int n,a = 0,temp = 0,i;
 	printf("Enter the integer:");
 	scanf("%d",&n);
 	a = n / 2;
 	for(i = 2; i<=a;i++){
 		if(n%i == 0){
 			printf("Not a prime!");
-			flag = 1;
+			temp = 1;
 			break;
 		}
 	}
-	if(flag == 0){
+	if(temp == 0){
 			printf("Prime!");
 		}
 	return 0;
