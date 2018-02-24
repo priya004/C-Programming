b383222163d3d8acbfb511bb97638c8eccb9211e
Jothi Priya Sundaravdivel <34646744+priya004@users.noreply.github.com>
2018-02-24 21:41:31 +0530

    Update Factorial.c

--- Codekata/Beginner/Set_2/Factorial.c
+++ Codekata/Beginner/Set_2/Factorial.c
@@ -2,23 +2,19 @@
 int main()
 {
     int n, i;
-    unsigned long long factorial = 1;
-
+    unsigned long long fact = 1;
     printf("Enter an integer: ");
     scanf("%d",&n);
-
     // show error if the user enters a negative integer
     if (n < 0)
         printf("Error! Factorial of a negative number doesn't exist.");
-
     else
     {
         for(i=1; i<=n; ++i)
         {
-            factorial *= i;              // factorial = factorial*i;
+            fact *= i;              // factorial = factorial*i;
         }
-        printf("Factorial of %d = %llu", n, factorial);
+        printf("Factorial of %d = %llu", n, fact);
     }
-
     return 0;
 }
