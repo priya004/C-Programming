dd3206fbfa9339f3b85526f95a82f388795d4ff0
Jothi Priya Sundaravdivel <34646744+priya004@users.noreply.github.com>
2018-02-24 22:08:01 +0530

    Update Reptitions.c

--- Codekata/Beginner/Set_12/Reptitions.c
+++ Codekata/Beginner/Set_12/Reptitions.c
@@ -1,1 +1,19 @@
-
+#include<stdio.h>
+int main(){
+  int N,K,A[100],i,count;
+  printf("\n Enter then number of elements in the array:");
+  scanf("%d",&N);
+  printf("\n Enter the value of K:");
+  scanf("%d",&K);
+  for(i = 0;i<N;i++){
+    scanf("%d",&A[i]);
+   }
+    for(i = 0;i<N;i++){
+      if(A[i] == K)
+        count++;
+      else
+        continue;
+     }
+    printf("%d",count);
+     return 0;
+}
