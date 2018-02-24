cfdc360f5b77fe70b5cf0a4fccaeacab20df092a
Jothi Priya Sundaravdivel <34646744+priya004@users.noreply.github.com>
2018-02-24 22:41:34 +0530

    Create Time_diff.c

---
+++ Codekata/Beginner/Set_3/Time_diff.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  int t1,t2,diff;
+  printf("\nEnter the first time:");
+  scanf("%d",&t1);
+  printf("\nEnter the second time:");
+  scanf("%d",&t2);
+  diff = t1-t2;
+  printf("\n%d",diff);
+  return 0;
+ }
