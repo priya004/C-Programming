55c73aa8acc70d7b5c948747cc1ddc849270ef4d
Jothi Priya Sundaravdivel <34646744+priya004@users.noreply.github.com>
2018-02-24 21:43:37 +0530

    Create Multiples.c

---
+++ Codekata/Beginner/Set_2/Multiples.c
@@ -0,0 +1,14 @@
+# include <stdio.h> 
+# include <conio.h>
+void main() 
+{ 
+ int i, n = 5, d ; 
+ clrscr() ; 
+ printf("\nEnter the number : ") ; 
+ scanf("%d", &d) ; 
+ printf("\nThe numbers divisible by %d are :\n\n", d) ; 
+ for(i = 1 ; i <= n ; i++) 
+  if(i % d == 0) 
+   printf("%d\t", i) ; 
+ getch() ; 
+}
